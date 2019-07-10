3942206ddd4877398e32b21930fb8ac96ab1c2e2
M. ZHANG <39518094+BASARANOMO@users.noreply.github.com>
2019-07-10 13:08:20 +0200

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 11:14:50 2019
+
+@author: ZHANG Mofan
+"""
+
+# Configuration file for StreetNetworkPreprocessing.py
+
+# Length minimum of road segments to be devided
+len_min = 50    # m
+
+# Are we going to mark down whether an intersection is created artificially or not?
+# yes == 1, no == 0
+true_inter_indicator = 0
